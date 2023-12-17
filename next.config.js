28b02409cda46b9945998ef0b9127046bd1f222c
shivanshu <bishtshivanshu@gmail.com>
2023-12-18 04:46:03 +0530

    enable typedRoutes and scrollRestoration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    scrollRestoration: true,
+    typedRoutes: true,
+  },
   images: {
     remotePatterns: [
       {

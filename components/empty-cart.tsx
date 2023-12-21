1eb72f98ed2eb1b520d5606ba490a85d0c570e89
shivanshu <bishtshivanshu@gmail.com>
2023-12-21 23:31:22 +0530

    fix loading and empty cart states

--- components/empty-cart.tsx
+++ components/empty-cart.tsx
@@ -10,15 +10,15 @@ export default function EmptyCart() {
   return (
     <div className="flex h-screen items-center justify-center bg-[var(--background-groww)]">
       <div className="flex flex-col items-center justify-center gap-4">
-        <ShoppingCartIcon className="h-24 w-24 bg-[var(--foreground-groww)]" />
-        <h2 className="bg-[var(--foreground-groww)] text-2xl">
+        <ShoppingCartIcon className="h-24 w-24 text-[var(--foreground-groww)]" />
+        <h2 className="text-2xl text-[var(--foreground-groww)]">
           Your Cart is Empty
         </h2>
-        <h6 className="bg-[var(--foreground-groww)] text-center">
+        <h6 className="text-center text-[var(--foreground-groww)]">
           You haven't added anything to your cart yet.
         </h6>
         <Button
-          className="mb-24 mt-4 w-full bg-[var(--primary-groww)]"
+          className="mb-24 mt-4 w-full bg-[var(--primary-groww)] text-[var(--primary-foreground-groww)]"
           onClick={() => router.refresh()}
         >
           Start Shopping

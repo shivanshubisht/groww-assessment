370b376791d3e59a6c5ef2b40334569dcf16b5e7
shivanshu <bishtshivanshu@gmail.com>
2023-12-21 21:43:04 +0530

    add pallete

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -5,11 +5,21 @@ export default function TestPage() {
     '--foreground-groww': 'hsl(0, 0%, 95%)',
     '--primary-groww': 'hsl(346.8, 77.2%, 49.8%)',
     '--primary-foreground-groww */}
-      <div className="w-100 h-100 bg-[var(--background-groww)]">test</div>
-      <div className="w-100 h-100 bg-[var(--foreground-groww)]">test</div>
-      <div className="w-100 h-100 bg-[var(--primary-groww)]">test</div>
+      bg-[var(--background-groww)]
+      <div className="w-100 h-100 bg-[var(--background-groww)]">
+        bg-[var(--background-groww)]
+      </div>
+      bg-[var(--foreground-groww)]
+      <div className="w-100 h-100 bg-[var(--foreground-groww)]">
+        bg-[var(--foreground-groww)]
+      </div>
+      bg-[var(--primary-groww)]
+      <div className="w-100 h-100 bg-[var(--primary-groww)]">
+        bg-[var(--primary-groww)]
+      </div>
+      bg-[var(--primary-foreground-groww)]
       <div className="w-100 h-100 bg-[var(--primary-foreground-groww)]">
-        test
+        bg-[var(--primary-foreground-groww)]
       </div>
     </>
   );
